c6131de2c49346c6b05d18b14b6a663874d3dbb6
Keith Burnell <keith@dotnetdevdude.com>
2012-05-17 15:57:52 -0500

    Custom Bindings

---
+++ Knockout/Knockout.Web/Scripts/ko-execute-on-enter.js
@@ -0,0 +1,17 @@
+﻿///<reference path="knockout-2.1.0.debug.js"/>
+///<reference path="jquery-1.7.2.min.js"/>
+
+ko.bindingHandlers.executeOnEnter = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        alert('you are here');
+        var value = valueAccessor();
+        $(element).keypress(function(event) {
+            var keyCode = (event.which ? event.which : event.keyCode);
+            if (keyCode === 13) {
+                value.call(viewModel);
+                return false;
+            }
+            return true;
+        });
+    }
+};
