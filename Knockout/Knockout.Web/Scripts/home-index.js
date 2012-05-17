de3eb3aaef39457429f6142a097d0af3e1d5c56c
Keith Burnell <keith@dotnetdevdude.com>
2012-05-17 10:21:56 -0500

    Parent Binding Contexts

--- Knockout/Knockout.Web/Scripts/home-index.js
+++ Knockout/Knockout.Web/Scripts/home-index.js
@@ -1,4 +1,5 @@
 ﻿///<reference path="knockout-2.1.0.debug.js"/>
+///<reference path="~/Scripts/jquery-1.7.2.min.js"/>
 
 $(function () {
     var data = [
@@ -15,10 +16,21 @@ $(function () {
     var viewModel = {
         manufacturers: ko.observableArray(data),
         manufacturerToAdd: ko.observable(""),
+        selectedManufacturer: ko.observable(null),
         addManufacturer: function () {
             this.manufacturers.push({ Name: this.manufacturerToAdd() });
             this.manufacturerToAdd("");
+        },
+        selectManufacturer: function () {
+            viewModel.selectedManufacturer(this);
         }
+        
     };
+
+    $(document).on("click", ".manufacturer-delete", function () {
+        var itemToRemove = ko.dataFor(this);
+        viewModel.manufacturers.remove(itemToRemove);
+    });
+
     ko.applyBindings(viewModel);
 });
